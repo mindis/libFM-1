843e576c11c26265d813a66ad2c001aaf9a11223
qdeee <qdeee@yandex-team.ru>
2014-04-01 14:50:47 +0400

    init commit: stream interaction, remove mandatory test file, etc

---
+++ src/fm_core/fm_data.h
@@ -0,0 +1,17 @@
+/*
+	Sparse data structure 
+
+	Author:   Steffen Rendle, http://www.libfm.org/
+	modified: 2012-01-04
+
+	Copyright 2010-2012 Steffen Rendle, see license.txt for more information
+*/
+
+#ifndef FM_DATA_H_
+#define FM_DATA_H_
+
+typedef float FM_FLOAT;
+typedef std::basic_istream<char, std::char_traits<char> > istream;
+typedef std::basic_ostream<char, std::char_traits<char> > ostream;
+
+#endif /*FM_DATA_H_*/
